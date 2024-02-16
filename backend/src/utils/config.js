47be3e5e453e50677b518267f376ff1a1ffa96e7
GemechisUrgessa <gemechis.urgessa@aait.edu.et>
2024-02-16 17:43:44 +0300

    bug fix

--- backend/src/utils/config.js
+++ backend/src/utils/config.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   try {
     // Note: This is not the best practice. Since there is sensitive information in the URI, I just put it here for the sake of the project.
     const Uri =
-      process.env.MONGO_URI || "mongodb://mongoadmin:secret@mongodb:27017/";
+      process.env.MONGO_URI || "mongodb://mongoadmin:secret@mongodb:27017";
     const conn = await mongoose.connect(Uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
