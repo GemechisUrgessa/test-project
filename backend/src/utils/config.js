cf258a5bc029b5f4a2d15e5576c1b83366f1c1ea
GemechisUrgessa <gemechis.urgessa@aait.edu.et>
2024-02-16 17:13:33 +0300

    final version

--- backend/src/utils/config.js
+++ backend/src/utils/config.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
+    // Note: This is not the best practice. Since there is sensitive information in the URI, I just put it here for the sake of the project.
     const Uri =
       process.env.MONGO_URI || "mongodb://mongoadmin:secret@mongodb:27017/";
     const conn = await mongoose.connect(Uri, {
